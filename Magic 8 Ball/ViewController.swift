255f23cddbd7ba4fa6b195ca4d9d84b312a9de42
Nabyl Bennouri <nabyl@liveutrahealthy.com>
2019-05-01 21:21:10 -0700

    screenshot

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var imageView: UIImageView!
+    var imageRandNumber: Int = 0
+    var imagesArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        newBallImage()
     }
 
-
+    @IBAction func askButtonPress(_ sender: Any) {
+        newBallImage()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
+    
+    
+    func newBallImage() {
+        imageRandNumber = Int.random(in: 0 ... 4)
+        imageView.image = UIImage(named: imagesArray[imageRandNumber])
+    }
 }
 
